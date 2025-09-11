796aa47bd1e0dfb36cd494c6d8e1f2402b300a64
Jon <jon.haataja@gmail.com>
2025-09-11 18:43:56 +0300

    Install Plus Jakarta Sans font

--- night-sky-app/src/App.tsx
+++ night-sky-app/src/App.tsx
@@ -1,4 +1,7 @@
 // import { useState } from 'react'
+// Supports weights 200-800
+//@ts-expect-error ts can't find font but it still works
+import '@fontsource-variable/plus-jakarta-sans';
 import './App.css'
 import ErrorBoundary from './ErrorBoundary';
 
