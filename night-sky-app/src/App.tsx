23f1beda2e2c992df0fa067beace467b40256486
Jon <jon.haataja@gmail.com>
2025-09-19 13:11:55 +0300

    Fix navbar label bug. Change paddings.

--- night-sky-app/src/App.tsx
+++ night-sky-app/src/App.tsx
@@ -52,7 +52,7 @@ function App() {
             <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>
           </svg>        
         </div>
-        <label className="logo">
+        <label className="logo" htmlFor="check">
         <img src={nightyLogo} alt="Nighty Logo" className='Nighty logo' />
         </label>
         <ul>
